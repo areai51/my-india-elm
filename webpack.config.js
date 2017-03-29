05de5dcca3f1bf6e2a16c9faf8a7ab3e8a769037
Kaustav Datta <kaustv.datta@gmail.com>
2017-03-29 20:07:41 +0530

    Initial commit
    Webpack + Elm configurations

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+  entry: {
+    app: [
+      './src/index.js'
+    ]
+  },
+
+  output: {
+    path: path.resolve(__dirname + '/dist'),
+    filename: '[name].js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        use: [{
+                loader: "style-loader"
+            }, {
+                loader: "css-loader"
+            }, {
+                loader: "sass-loader",
+                options: {
+                    includePaths: [path.resolve(__dirname, 'node_modules/bootstrap/scss/')]
+                }
+            }]
+      },
+      {
+        test:    /\.html$/,
+        exclude: /node_modules/,
+        loader:  'file-loader?name=[name].[ext]',
+      },
+      {
+        test:    /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        loader:  'elm-webpack-loader?verbose=true&warn=true',
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
+      }
+    ],
+
+    noParse: /\.elm$/,
+  },
+
+  plugins: [
+        new webpack.ProvidePlugin({
+           $: "jquery",
+           jQuery: "jquery"
+       })
+    ],
+
+  devServer: {
+    inline: true,
+    stats: { colors: true },
+  },
+
+
+};
