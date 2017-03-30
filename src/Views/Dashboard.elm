e2da44a7ca68c4e85862142cdbbceac348286d2b
Kaustav Datta <kaustv.datta@gmail.com>
2017-03-30 20:31:37 +0530

    Side Nav added

--- src/Views/Dashboard.elm
+++ src/Views/Dashboard.elm
@@ -1,7 +1,7 @@
 module Views.Dashboard exposing (..)
 
-import Html exposing (Html, div, text, nav, img, a)
-import Html.Attributes exposing (class, href, src, height, width)
+import Html exposing (Html, div, text, nav, img, a, button)
+import Html.Attributes exposing (class, href, src, height, width, style)
 import Msgs exposing (Msg)
 import Models exposing (Model)
 import Views.Leaders.List
@@ -9,14 +9,24 @@ import Views.Leaders.List
 
 view : Model -> Html Msg
 view model =
-    nav [ class "navbar navbar-inverse bg-inverse" ]
+    div [ class "container-fluid" ] [
+        nav [ class "navbar navbar-inverse bg-inverse" ]
             [ a [ class "navbar-brand", href "#" ] 
                 [ img [ src "http://seeklogo.com/images/E/elm-logo-9DEF2A830B-seeklogo.com.png", 
                         height 60,
                         width 60 ] 
                     [] 
                 ] 
-            ]
+            ],
+        nav [ class "nav flex-column" , style [ ("width", "15%"), ("float", "left") ] ] [
+            a [ class "nav-link", href "#" ] [ text "Our Leaders" ],
+            a [ class "nav-link", href "#" ] [ text "Crime in States" ],
+            a [ class "nav-link", href "#" ] [ text "Accidents in Cities" ],
+            a [ class "nav-link", href "#" ] [ text "Our Rivers" ]
+        ],
+        div [ class "container" , style [ ("width", "85%"), ("float", "left") ] ] []
+    ]
+
 
 
 page : Model -> Html Msg
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ require('bootstrap/dist/css/bootstrap.css');
 require('./index.html');
 
 var Elm = require('./Main.elm');
-var mountNode = document.getElementById('main');
+var mountNode = document.body;
 
 // .embed() can take an optional second argument. This would be an object describing the data we need to start a program, i.e. a userID or some token
 var app = Elm.Main.embed(mountNode);
